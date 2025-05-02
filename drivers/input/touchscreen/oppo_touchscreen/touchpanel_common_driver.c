6c5cd86f7e2479273f98b3af59ff88faaa1fa48d
Ipin22 <ucihamala573@gmail.com>
2025-05-02 19:59:00 +0000

    Revert "touchpanel: always load firmware from /vendor/firmware via OPPO_FW_DIR"
    This reverts commit e0efec05945b7dc8a1439cf9fb57b96efb363554.

--- drivers/input/touchscreen/oppo_touchscreen/touchpanel_common_driver.c
+++ drivers/input/touchscreen/oppo_touchscreen/touchpanel_common_driver.c
@@ -21,7 +21,6 @@
 #include <linux/syscalls.h>
 #include <linux/timer.h>
 #include <linux/of_gpio.h>
-#define OPPO_FW_DIR "/vendor/firmware/"
 #include <linux/time.h>
 #include <linux/delay.h>
 #include <linux/regulator/consumer.h>
@@ -1082,16 +1081,16 @@ static void tp_fw_update_work(struct work_struct *work)
                     strlcat(fw_name_fae, postfix, MAX_FW_NAME_LENGTH);
                     strlcat(fw_name_fae, p_node, MAX_FW_NAME_LENGTH);
                     TPD_INFO("fw_name_fae is %s\n", fw_name_fae);
-                    ret = request_firmware(&fw, OPPO_FW_DIR,  fw_name_fae, ts->dev);
+                    ret = request_firmware(&fw, fw_name_fae, ts->dev);
                     if (!ret)
                         break;
                 } else {
-                    ret = request_firmware(&fw, OPPO_FW_DIR, ts->panel_data.fw_name, ts->dev);
+                    ret = request_firmware(&fw, ts->panel_data.fw_name, ts->dev);
                     if (!ret)
                         break;
                 }
             } else {
-                ret = request_firmware_select(&fw, OPPO_FW_DIR, ts->panel_data.fw_name, ts->dev);
+                ret = request_firmware_select(&fw, ts->panel_data.fw_name, ts->dev);
                 if (!ret)
                     break;
             }
